2589edd8a20a0802ed491ec8f4f853c18cd0891a
Tim Dettmers <tim.dettmers@gmail.com>
2020-09-23 20:16:35 -0700

    Replaced Qt4 with tkinter for speed; added window names.

--- alttab.py
+++ alttab.py
@@ -1,30 +1,113 @@
 from __future__ import print_function
 import keyboard
 import sys
-from PyQt4 import QtGui, QtCore
 from winlaunch import *
 from threading import Thread, Event
 import time
 import copy
 from util import Timer
 import itertools
-# Press PAGE UP then PAGE DOWN to type "foobar".
+import tkinter as tk
+import tkinter.font as font
+
 
 t = Timer()
 
-num_multi_keys = 2
-keys = [key for key in 'jkfdlshga;utreiwop']
-scan_codes = [74, 75, 70, 68, 76, 83, 72, 71, 65, 59, 85, 84, 82, 69, 73, 87,79,80]
+num_multi_keys = 1
+keys = [key for key in 'jkfdlshga;utreiwopqmnbvcxz']
+scan_codes = [74, 75, 70, 68, 76, 83, 72, 71, 65, 59, 85, 84, 82, 69, 73, 87,79,80, 81, 77, 78, 66, 86, 67, 88, 90]
 main_keys = keys[:num_multi_keys]
 secondary_keys = keys[num_multi_keys:]
 main_codes = scan_codes[:num_multi_keys]
 secondary_codes = scan_codes[num_multi_keys:]
 multi_keys = [r for r in itertools.product(main_keys, secondary_keys)]
 multi_codes = [r for r in itertools.product(main_codes, secondary_codes)]
-keys = keys[num_multi_keys:] + multi_keys
-scan_codes = scan_codes[num_multi_keys:] + multi_codes
+keys = keys[num_multi_keys:] #+ multi_keys
+scan_codes = scan_codes[num_multi_keys:] #+ multi_codes
 
-polling_interval = 0.5
+
+polling_interval = 2.0
+
+class KeyDisplay:
+    def __init__(self, master, text):
+        self.master = master
+        self.text = tk.StringVar()
+        self.text.set(text)
+        self.frame = tk.Frame(self.master, width=40, height=40)
+        self.lbl = tk.Label(self.frame, textvariable=self.text, bg='#3AD7AF', fg='#474640', height=1, width=10)
+        self.lbl['font'] = font.Font(size=15)
+        self.lbl.pack()
+        self.frame.pack()
+        self.master.overrideredirect(1)
+        self.master.geometry('+0+0')
+        self.master.withdraw()
+
+    def close_windows(self):
+        self.master.destroy()
+
+
+
+class WindowManager:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master, width=40, height=40)
+        self.button1 = tk.Button(self.frame, text = 'Close', width = 5, command = self.close)
+        self.button1.pack()
+
+        self.master.bind("<KeyPress>", self.select_via_key)
+
+        self.frame.pack()
+        self.windows = []
+        self.key2window = {}
+        self.event = False
+        for mkeys in keys:
+            text = ''.join(mkeys)
+            newWindow = tk.Toplevel(self.master)
+
+            window = KeyDisplay(newWindow, text)
+            self.key2window[mkeys] = window
+
+        self.moved_windows = []
+        self.key2wid = {}
+
+    def move(self, key, x, y, name):
+        window = self.key2window[key]
+        chunks = name.split(' ')[::-1]
+        app_text = ''
+        while len(app_text) < 10:
+            chunk = chunks.pop(0)
+            if chunk == '-': continue
+            if chunk == '|': continue
+            if len(chunk) + len(app_text) > 10: break
+            app_text = chunk + app_text
+
+
+        window.text.set('{0} {1}'.format(key, app_text))
+        window.master.geometry('+{0}+{1}'.format(int(x), int(y)))
+        window.master.deiconify()
+        #window.master.update()
+        self.moved_windows.append(window)
+
+
+    def reset(self):
+        for window in self.moved_windows:
+            #window.master.geometry('+0+0')
+            window.master.withdraw()
+        self.moved_windows = []
+        self.event = False
+
+    def close(self):
+        self.master.destroy()
+
+    def select_via_key(self, event):
+        if self.event:
+            if event.char in self.key2wid:
+                focus(self.key2wid[event.char])
+                self.reset()
+
+
+
+
 
 class WindowPoller(Thread):
     def __init__(self):
@@ -75,18 +158,29 @@ class WindowPoller(Thread):
                 if 'alttab.py' == name: continue
                 if pos is None: continue
                 if 'unity' in name: continue
+                if 'tk' in name: continue
+                if 'launcher' in name: continue
                 if 'Desktop' in name: continue
+                pos = list(pos)
+                size = list(size)
                 if pos[0] < 0 or pos[1] < 0: continue # todo: handle windows on other desktops
                 if size is None: continue
                 centerx = (pos[0]+size[0])-(size[0]/2)
                 centery = (pos[1]+size[1])-(size[1]/2)
                 p = [centerx, centery]
-                new_widget_params.append([centerx, centery, keys[j], wid, scan_codes[j]])
+                new_widget_params.append([centerx, centery, keys[j], wid, scan_codes[j], name])
             if error:
                 time.sleep(polling_interval)
                 continue
             self.widget_params = new_widget_params
             time.sleep(polling_interval)
+
+
+
+root = tk.Tk()
+root.attributes("-alpha", 0.0)
+manager = WindowManager(root)
+
 
 class KeyEvent:
     key2wid = None
@@ -97,109 +191,58 @@ class KeyEvent:
     def add_key(key):
         if key in KeyEvent.key2wid:
             KeyEvent.key1 = key
-            QtGui.qApp.quit()
+            manager.reset()
         else:
             if KeyEvent.key1 is None:
                 KeyEvent.key1 = key
             else:
                 KeyEvent.key2 = key
-                QtGui.qApp.quit()
+                manager.reset()
 
-
-
-
-
-
-nested_keys = []
 
 def handle_key_event():
+
+    #wids = current_windows()
+    #widgets = []
+    #key2wid = {}
+    #params = []
+    #for j, wid in enumerate(wids):
+    #    pos, size, name = win_pos(wid), win_size(wid), win_name(wid)
+    #    if 'alttab.py' == name: continue
+    #    if pos is None: continue
+    #    if 'unity' in name: continue
+    #    if 'tk' in name: continue
+    #    if 'launcher' in name: continue
+    #    if 'Desktop' in name: continue
+    #    pos = list(pos)
+    #    size = list(size)
+    #    if pos[0] < 0 or pos[1] < 0: continue # todo: handle windows on other desktops
+    #    if size is None: continue
+    #    centerx = (pos[0]+size[0])-(size[0]/2)
+    #    centery = (pos[1]+size[1])-(size[1]/2)
+    #    params.append([centerx, centery, keys[j], wid, scan_codes[j], name])
+
+    manager.event = True
     t.tick('full')
-    app = QtGui.QApplication(sys.argv)
     params = copy.deepcopy(p.widget_params)
     p.key_event_start()
     if len(params) == 0:
         p.key_event_stop()
         return
     widgets = []
-    key2wid = {}
-    t.tick('generate windows')
-    for x, y, key, wid, scan_code in params:
-        widgets.append(mymainwindow(x, y, key))
-        key2wid[scan_code] = wid
-        t.tick('show windows')
-        widgets[-1].show()
-        t.tick('show windows')
-    t.tock('generate windows')
-    t.tock('show windows')
-
-    KeyEvent.key2wid = key2wid
-    t.tick('exec app')
-    app.exec_()
-    t.tock('exec app')
-
-    if KeyEvent.key1 in key2wid:
-        focus(key2wid[KeyEvent.key1])
-    if (KeyEvent.key1, KeyEvent.key2) in key2wid:
-        focus(key2wid[(KeyEvent.key1, KeyEvent.key2)])
-    t.tock('full')
-
-    KeyEvent.key1 = None
-    KeyEvent.key2 = None
+    coords = set()
+    for x, y, key, wid, scan_code, name in params:
+        while (x,y) in coords:
+            y += 40
+            x += 40
+        coords.add((x,y))
+        manager.move(key, x, y, name)
+        manager.key2wid[key] = wid
     p.key_event_stop()
-
-
-
-
-class mymainwindow(QtGui.QLabel):
-    def __init__(self, x, y, text):
-        QtGui.QWidget.__init__(self)
-        self.setWindowFlags(
-            QtCore.Qt.WindowStaysOnTopHint |
-            QtCore.Qt.FramelessWindowHint
-            )
-        #self.setLineWrapMode(QtGui.QTextEdit.FixedPixelWidth)
-        self.setGeometry(x,y, 40,40)
-        font = self.font()
-        font.setFamily("Courier")
-        font.setPointSize(20)
-        fgcolor = QtGui.QColor('#474640')
-        bgcolor = QtGui.QColor('#3AD7AF')
-        P = self.palette()
-        P.setColor(QtGui.QPalette.Background, bgcolor)
-        P.setColor(QtGui.QPalette.Foreground, fgcolor)
-        self.setPalette(P)
-
-        self.setFont(font)
-        if isinstance(text, tuple):
-            text = "".join(text)
-            self.num_keys = 2
-        else:
-            self.num_keys = 1
-        self.setText(text)
-        self.setAlignment(QtCore.Qt.AlignCenter)
-        self.num_key_events = 0
-        self.text = text
-
-
-        #QtCore.QTimer.singleShot(300, self.close)
-
-    def center(self):
-        frameGm = self.frameGeometry()
-        screen = QtGui.QApplication.desktop().screenNumber(QtGui.QApplication.desktop().cursor().pos())
-        centerPoint = QtGui.QApplication.desktop().screenGeometry(screen).center()
-        frameGm.moveCenter(centerPoint)
-        centerPoint = QtCore.QPoint(centerPoint.x()+self.offset, centerPoint.y())
-        self.move(centerPoint)
-
-    def mousePressEvent(self, event):
-        QtGui.qApp.quit()
-
-    def keyPressEvent(self, e):
-        KeyEvent.add_key(e.key())
-
+    manager.master.focus_force()
 
 
 p = WindowPoller()
 p.start()
 keyboard.add_hotkey('ctrl+alt+k', handle_key_event)
-keyboard.wait()
+root.mainloop()
